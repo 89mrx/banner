7cb211f4f9b2f5caec0acdc9fad1c7bba072fa22
89mrx <77620490+89mrx@users.noreply.github.com>
2021-01-22 16:30:48 +0100

    Create banner.sh

---
+++ banner.sh
@@ -0,0 +1,10 @@
+clear
+echo                                                                          echo                                                          
+cowsay -f logo.py WELCOME 89MRX | lolcat
+echo
+toilet -f standard -F gay "89MRX"
+echo
+neofetch
+echo -e '\033[01;32m'
+date
+echo -e '\033[01;32m'
